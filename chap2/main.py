712cc87c6f75952579f1aaf6c3089ae078daf4d0
Jeddie Vedder <jeddievedder@jedcn.com>
2024-05-24 19:47:05 -0400

    chap2 f"{}" w/ function

--- chap2/main.py
+++ chap2/main.py
@@ -7,4 +7,6 @@ print(name.lower())
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-print(full_name)
+print(full_name.title())
+
+print(f"Hello {full_name.upper()} !")
