b66ab1eac195f44f6493c4132daadecc15f5480b
Jeddie Vedder <jeddievedder@jedcn.com>
2024-05-29 14:23:21 -0400

    chap2 removeprefix, removesuffix

--- chap2/main.py
+++ chap2/main.py
@@ -10,3 +10,8 @@ full_name = f"{first_name} {last_name}"
 print(full_name.title())
 
 print(f"Hello {full_name.upper()} !")
+
+print("   what do you know   ".lstrip())
+print("   what do you know   ".rstrip())
+
+print("https://www.duckduckgo.com/".removeprefix("https://").removesuffix("/"))
