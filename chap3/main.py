61d1e226006da4dad18abf907f443bfc2572548b
Jeddie Vedder <jeddievedder@jedcn.com>
2024-05-30 16:36:17 -0400

    chap3 list append, insert

--- chap3/main.py
+++ chap3/main.py
@@ -3,3 +3,13 @@ print(bicycles[0].upper())
 
 last_item = bicycles[-1]
 second_to_last = bicycles[-2]
+
+motorcycles = [ 'honda', 'yamaha', 'suzuki' ]
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+motorcycles.append('honda')
+print(motorcycles)
+
+motorcycles.insert(0, 'bmw')
+print(motorcycles)
